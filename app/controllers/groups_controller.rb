635abc18a3dbf6ed9fb0fc2c7d6ed825e3c1da06
qinfeng8848 <q884822@gmail.com>
2018-07-26 18:02:36 +0800

    只有建群者,才能编辑群信息

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -42,11 +43,18 @@ class GroupsController < ApplicationController
     def destroy
       @group = Group.find(params[:id])
       @group.destroy
-      flash[:alert] = "Group deleted 群已经删除"
+      flash[:alert] = "Group deleted 群已删除"
       redirect_to groups_path
     end
 
     private
+
+    def find_group_and_check_permission
+      @group = Group.find(params[:id])
+      if current_user != @group.user
+         redirect_to root_path, alert: "你没有权限操作!"
+      end
+    end
 
     def group_params
       params.require(:group).permit(:title, :description)
