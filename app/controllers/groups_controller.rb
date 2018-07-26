59364a3c90373f4ee71b6f18ea7163a36dc90c6b
qinfeng8848 <q884822@gmail.com>
2018-07-26 16:29:31 +0800

    建议用户和群的关系

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,8 +15,9 @@ class GroupsController < ApplicationController
 
     def create
       @group = Group.new(group_params)
-
+      @group.user = current_user
       if @group.save
+
         redirect_to groups_path
       else
         render :new
